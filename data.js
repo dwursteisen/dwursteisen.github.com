2717a6bc909bdc163d512c16f94d526e955f00a3
dwursteisen <david.wursteisen+git@gmail.com>
2012-11-16 23:40:30 +0100

    Externalisation des données

---
+++ data.js
@@ -0,0 +1,49 @@
+window.AllDataAreBelongToUs = [
+    {
+        categorie_id:'empty',
+        titre:'Salut !',
+        contenu:'Passe ta souris sur une catégorie qui t\'intéresse pour en savoir plus !',
+        visible:true},
+    {
+        categorie_id:'projets',
+        titre:'image-api',
+        contenu:'An easy-to-use java library for imgur, flickr and facebook. And all of this with OAuth support !',
+        visible:false,
+        liens:'https://github.com/dwursteisen/image-api'
+    },
+    {
+        categorie_id:'projets',
+        titre:'gp-parser',
+        contenu:'Guitar Pro file parser (in Java)',
+        visible:false,
+        liens:'https://github.com/dwursteisen/gp-parser'
+    },
+    {
+        categorie_id:'presentations',
+        titre:'TDD Quest',
+        contenu:"Vous subissez les régressions à chaque livraison ? Vous ne voyez pas l’intéret des tests unitaires car ils ne servent qu’à tester des additions ? Les tests ne sont pas applicables à votre projet car il est trop complexe ? Si c’est le cas, suivez moi dans la quête du Test Driven Development. Nous rencontrerons pléthore d'ennemis contre lesquels nous aurons à combattre : bugs, complexité, code statique, couplage fort. Ils essaieront de nous barrer la route, mais heureusement, nous pourrons compter sur nos alliés jUnit, Mockito, refactoring et injection/dépendance.",
+        visible:false,
+        liens:'https://speakerdeck.com/dwursteisen/tdd-quest'
+    },
+    {
+        categorie_id:'articles',
+        titre:'7 things to make good team great',
+        contenu:'7 petits trucs mis en place chez Atlassian pour booster son équipe',
+        liens:'http://blog.soat.fr/2012/11/devoxx-2012-7-things-to-make-good-team-great/',
+        visible:false
+    },
+    {
+        categorie_id:'articles',
+        titre:'JSR 308 – Annotations On Java Types',
+        contenu:'Évolution sur les annotations dans Java 8',
+        liens:'http://blog.soat.fr/2012/11/devoxx-2012-jsr-308-annotations-on-java-types/',
+        visible:false
+    },
+    {
+        categorie_id:'articles',
+        titre:'JSR 353 : Java API for JSON Processing',
+        contenu:'API pour produire/consomer du json via Java EE 7',
+        liens:'http://blog.soat.fr/2012/11/devoxx-2012-jsr-353-java-api-for-json-processing/',
+        visible:false
+    }
+];
