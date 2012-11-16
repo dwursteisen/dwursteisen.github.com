68f8766a76b839f1b8895dca09271cf6e9f35188
dwursteisen <david.wursteisen+git@gmail.com>
2012-11-16 23:20:18 +0100

    Petit changement de présenation

--- website.js
+++ website.js
@@ -1,6 +1,6 @@
 function Website($scope) {
 
-    $scope.titre = 'Mon site web personnel !';
+    $scope.titre = 'Mes trucs';
 
     $scope.categories = [
         {nom:'Projets', id:'projets', icon:'icon-hdd'},
@@ -9,9 +9,29 @@ function Website($scope) {
     ];
 
     $scope.items = [
-        {categorie_id:'projets', titre:'image-api', contenu:'blablabla', visible:true},
-        {categorie_id:'projets', titre:'gp-parser', contenu:'blablabla', visible:true},
-        {categorie_id:'presentations', titre:'TDD Quest', contenu:'blablabla', visible:true}
+
+        {categorie_id:'empty', titre:'Salut !',
+            contenu:'Passe ta souris sur une catégorie qui t\'intéresse pour en savoir plus !',
+            visible:true,
+            liens:'https://github.com/dwursteisen/image-api'},
+        {categorie_id:'projets', titre:'image-api', contenu:'blablabla', visible:false,
+            liens:'https://github.com/dwursteisen/image-api'},
+        {categorie_id:'projets', titre:'gp-parser', contenu:'blablabla', visible:false,
+            liens:'https://github.com/dwursteisen/gp-parser'},
+        {categorie_id:'presentations', titre:'TDD Quest', contenu:'blablabla', visible:false,
+            liens:'https://speakerdeck.com/dwursteisen/tdd-quest'} ,
+        {categorie_id:'articles',
+            titre:'7 things to make good team great',
+            contenu:'blabla',
+            liens:'http://blog.soat.fr/2012/11/devoxx-2012-7-things-to-make-good-team-great/', visible:false},
+        {categorie_id:'articles',
+            titre:'JSR 308 – Annotations On Java Types',
+            contenu:'blabla',
+            liens:'http://blog.soat.fr/2012/11/devoxx-2012-jsr-308-annotations-on-java-types/', visible:false},
+        {categorie_id:'articles',
+            titre:'JSR 353 : Java API for JSON Processing',
+            contenu:'blabla',
+            liens:'http://blog.soat.fr/2012/11/devoxx-2012-jsr-353-java-api-for-json-processing/', visible:false}
     ];
 
     $scope.affiche = function (categorie_id) {
@@ -19,4 +39,6 @@ function Website($scope) {
             item.visible = (item.categorie_id === categorie_id);
         });
     }
+
+    $scope.affiche('empty');
 }
